67fa9562d2f0103f56dc6e969a98a674985866f0
ianpemb01 <ianpemb01@gmail.com>
2024-11-18 14:12:27 -0500

    Added s too http.

--- Capstone/src/components/fetchImagesComponent.vue
+++ Capstone/src/components/fetchImagesComponent.vue
@@ -9,7 +9,7 @@ const loading = ref(false);
 loading.value = true;
 try {
   //   loading.value = true;
-  axios.get("http://generative-image-a-i-express-40t73jy4w.vercel.app/gallery").then((json) => {
+  axios.get("https://generative-image-a-i-express-40t73jy4w.vercel.app/gallery").then((json) => {
     console.log({ taco: json.data });
     images.value = json.data;
   });
